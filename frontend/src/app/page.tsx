4f6dc88af9bea6f72fa355cd8412ca5f280fcc17
guboc11 <guboc11@gmail.com>
2024-11-09 17:53:51 +0900

    Add a new page and implement routing for it.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,6 +9,10 @@ export default function Home() {
       <p className="text-2xl">Here is Main page(2xl)</p>
       <p className="text-3xl">Here is Main page(3xl)</p>
       <p className="text-4xl">Here is Main page(4xl)</p>
+      <a className="text-blue-600" href="https://tailwindcss.com/docs/font-size">Tailwind Docs Link</a>
+      <div>
+        <Link className="text-yellow-700 font-bold" href={'/bankok'}>Go To Bankok Page</Link>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/pages/bankok/index.tsx
@@ -0,0 +1,9 @@
+const Bankok = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-green-100">
+      <h1 className="text-3xl font-bold">Welcome to Bankok Page</h1>
+    </div>
+  );
+};
+
+export default Bankok;
